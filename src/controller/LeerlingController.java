08f77841881149b2073056e1409644e265a10efa
Matthias <Matthias@Matthias-PC>
2016-02-18 12:02:04 +0100

    GUI veld #13 Info Leerling aangemaakt. Toepassen MVC op InfoScherm & LeerlingInfoHouder

---
+++ src/controller/LeerlingController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import domein.Leerling;
+
+/**
+ *
+ * @author Matthias
+ */
+public class LeerlingController {
+    private final Leerling leerling;
+    
+    public LeerlingController(Leerling l){
+        this.leerling=l;
+    }
+    
+    
+    
+}
