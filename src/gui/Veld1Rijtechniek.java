43631e7d82de15447597729752dcef0f6c07fe6d
simon <simon@192.168.0.109>
2016-02-23 18:39:19 +0100

    GridPane links, rechts en het stuur ingevuld met afbeeldingen

--- src/gui/Veld1Rijtechniek.java
+++ src/gui/Veld1Rijtechniek.java
@@ -46,6 +46,27 @@ public class Veld1Rijtechniek extends GridPane{
         stuur1Img = new ImageView("resource/Rijtechniek/stuur1Neutraal.png");
         stuur2Img = new ImageView("resource/Rijtechniek/stuur2Neutraal.png");
         stuur3Img = new ImageView("resource/Rijtechniek/stuur3Neutraal.png");
+        
+        //GridPane links
+        links.add(remImg, 0, 0);
+        links.add(koppelingImg, 0, 1);
+        links.add(houdingImg, 0, 2);
+        links.add(hellingImg, 0, 3);
+        links.add(stuuroefImg, 0, 4);
+        links.add(achteruitImg, 0, 5);
+        
+        //GridPane rechts
+        rechts.add(stuurImg, 0, 0);
+        rechts.add(schakelImg, 0, 1);
+        rechts.add(kijkImg, 0, 2);
+        rechts.add(parkerenImg, 0, 3);
+        rechts.add(kerenImg, 0, 4);
+        rechts.add(garageImg, 0, 5);
+        
+        //GridPane stuurgp
+        stuurGp.add(stuur1Img, 0, 0, 2, 1);
+        stuurGp.add(stuur2Img, 0, 1);
+        stuurGp.add(stuur3Img, 1, 1);
     }
     
 }
