c3d8371b372ae6cfd0cb692e0fdcaee6db054e7a
simon <simon@192.168.0.109>
2016-03-01 17:14:47 +0100

    Leerlingen controller uitgebreid en nodige functionaliteiten van getters en setters gegeven

--- src/controller/LeerlingController.java
+++ src/controller/LeerlingController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import domein.EvaluatieMoment;
 import domein.Leerling;
+import java.util.Date;
+import javafx.scene.image.Image;
 
 /**
  *
@@ -11,6 +14,87 @@ public class LeerlingController {
     
     public LeerlingController(Leerling l){
         this.leerling=l;
+    }
+    
+    public EvaluatieMoment getEva1() {
+        return leerling.getEva1();
+    }
+    
+    public void setEva1(EvaluatieMoment eva1) {
+        leerling.setEva1(eva1);
+    }
+
+    public EvaluatieMoment getEva2() {
+        return leerling.getEva2();
+    }
+
+    public void setEva2(EvaluatieMoment eva2) {
+        leerling.setEva2(eva2);
+    }
+
+    public EvaluatieMoment getEva3() {
+        return leerling.getEva3();
+    }
+
+    public void setEva3(EvaluatieMoment eva3) {
+        leerling.setEva3(eva3);
+    }
+
+    public int getLastSelectedEva() {
+        return leerling.getLastSelectedEva();
+    }
+
+    public void setLastSelectedEva(int lastSelectedEva) {
+        leerling.setLastSelectedEva(lastSelectedEva);
+    }
+   
+
+    public String getNaam() {
+        return leerling.getNaam();
+    }
+
+    public Image getImage() {
+        return leerling.getImage();
+    }
+
+    public void setImage(Image image) {
+        leerling.setImage(image);
+    }
+
+    public void setNaam(String naam) {
+        leerling.setNaam(naam);
+    }
+
+    public String getInschrijvingsnr() {
+        return leerling.getInschrijvingsnr();
+    }
+
+    public void setInschrijvingsnr(String inschrijvingsnr) {
+        leerling.setInschrijvingsnr(inschrijvingsnr);
+    }
+
+    public String getInstructeur() {
+        return leerling.getInstructeur();
+    }
+
+    public void setInstructeur(String instructeur) {
+        leerling.setInstructeur(instructeur);
+    }
+
+    public Date getVerval() {
+        return leerling.getVerval();
+    }
+
+    public void setVerval(Date verval) {
+        leerling.setVerval(verval);
+    }
+
+    public String getType() {
+        return leerling.getType();
+    }
+
+    public void setType(String type) {
+        leerling.setType(type);
     }
     
     
