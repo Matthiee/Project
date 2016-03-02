3a4f40e9f893bd8c641ed77fc625354979625bad
simon <simon@192.168.0.109>
2016-03-02 01:03:42 +0100

    Klasse evaselector uiterlijk gemaakt

--- src/gui/EvaSelector.java
+++ src/gui/EvaSelector.java
@@ -3,6 +3,8 @@ package gui;
 
 import controller.EvaController;
 import controller.LeerlingController;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
@@ -25,11 +27,17 @@ public class EvaSelector extends VBox{
         lbl3.setMaxSize(30, 30);
         lbl3.setMinSize(30, 30);
         
-        lbl1.setStyle("-fx-background-color: WHITE");
-        lbl2.setStyle("-fx-background-color: WHITE");
-        lbl3.setStyle("-fx-background-color: WHITE");
+        lbl1.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: WHITE");
+        lbl2.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: WHITE");
+        lbl3.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: WHITE");
         
+        lbl1.setAlignment(Pos.CENTER);
+        lbl2.setAlignment(Pos.CENTER);
+        lbl3.setAlignment(Pos.CENTER);
+        
+        this.setAlignment(Pos.CENTER);
         this.getChildren().addAll(lbl1, lbl2, lbl3);
+        this.setSpacing(8);
         
         update();
         
@@ -55,13 +63,13 @@ public class EvaSelector extends VBox{
     }
     
     public void update(){
-        lbl1.setStyle("-fx-background-color: WHITE");
-        lbl2.setStyle("-fx-background-color: WHITE");
-        lbl3.setStyle("-fx-background-color: WHITE");
+        lbl1.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: WHITE");
+        lbl2.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: WHITE");
+        lbl3.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: WHITE");
         
-        if(evaCtrl.getSelectedEva() == 1)lbl1.setStyle("-fx-background-color: GREEN");
-        if(evaCtrl.getSelectedEva() == 2)lbl2.setStyle("-fx-background-color: GREEN");
-        if(evaCtrl.getSelectedEva() == 3)lbl3.setStyle("-fx-background-color: GREEN");
+        if(evaCtrl.getSelectedEva() == 1)lbl1.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: GREEN");
+        if(evaCtrl.getSelectedEva() == 2)lbl2.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: GREEN");
+        if(evaCtrl.getSelectedEva() == 3)lbl3.setStyle("-fx-border-color: black; -fx-border-width:1; -fx-border-style: solid; -fx-background-color: GREEN");
     }
     
 }
