d8c028f1620c3358d5517941dba9b3114a952003
Simon De Spiegeleer <simondespiegeleer@msn.com>
2016-04-06 18:28:50 +0200

    VerkeerstechniekAfstand persistentie toegevoegd

--- src/gui/Veld2VerkeerstechniekAfstand.java
+++ src/gui/Veld2VerkeerstechniekAfstand.java
@@ -1,5 +1,7 @@
 package gui;
 
+import controller.EvaController;
+import controller.LeerlingController;
 import controller.SchermController;
 import domein.Verkeerstechniek;
 import javafx.collections.FXCollections;
@@ -19,9 +21,11 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
-public class Veld2VerkeerstechniekAfstand extends Pane {
+public class Veld2VerkeerstechniekAfstand extends Pane implements View{
 
     private final SchermController schermController;
+    private EvaController evaController;
+    private LeerlingController llnController;
     private Button exit;
     private TableView<String> list = new TableView<String>();
     private ObservableList<String> houdingen = FXCollections.observableArrayList();
@@ -32,13 +36,13 @@ public class Veld2VerkeerstechniekAfstand extends Pane {
 
     private TableView<Verkeerstechniek> table = new TableView<Verkeerstechniek>();
     private ObservableList<Verkeerstechniek> data
-            = FXCollections.observableArrayList(
-                    new Verkeerstechniek("Volgafstand", ""),
-                    new Verkeerstechniek("Zijdelingse afstand", "")
-            );
+            = FXCollections.observableArrayList();
 
-    public Veld2VerkeerstechniekAfstand(SchermController schermCtrl) {
+    public Veld2VerkeerstechniekAfstand(SchermController schermCtrl, EvaController evaCtrl, LeerlingController llnCtrl) {
+        evaController = evaCtrl;
         schermController = schermCtrl;
+        llnController = llnCtrl;
+        this.llnController.getLeerling().addView(this);
         vBox1.getChildren().addAll(toon);
         houdingen.addAll("Volgafstand", "Zijdelingse afstand");
 
@@ -116,6 +120,7 @@ public class Veld2VerkeerstechniekAfstand extends Pane {
         exit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
+                evaController.saveListDataVerkeerstechniek("Afstand", data);
                 Veld2VerkeerstechniekAfstand.this.schermController.setScherm(MainApp.VERKEERSTECHNIEK_ID);
             }
         });
@@ -126,6 +131,7 @@ public class Veld2VerkeerstechniekAfstand extends Pane {
 
         this.getChildren().addAll(vBox1, vBox2, hBox1);
         this.setMinWidth(600);
+        update();
     }
 
     private void doorgaanAlsGebruikerGeselecteerd() {
@@ -135,5 +141,11 @@ public class Veld2VerkeerstechniekAfstand extends Pane {
 
     private String verkort(String s) {
         return s.replaceAll("(.{37})", "$1\n");
+    }
+
+    @Override
+    public void update() {
+        data = evaController.loadListDataVerkeerstechniek("Afstand");
+        table.setItems(data);
     }
 }
