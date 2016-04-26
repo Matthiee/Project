ceddf0143b07d086521de517bc68152683253f97
Simon De Spiegeleer <simondespiegeleer@msn.com>
2016-04-26 17:38:23 +0200

    rijtechhniek andere klassen uiterlijk aangepast

--- src/main/java/be/matkensim/project/gui/Veld1RijtechniekKeren.java
+++ src/main/java/be/matkensim/project/gui/Veld1RijtechniekKeren.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -18,16 +19,16 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
-public class Veld1RijtechniekKeren extends Pane implements View{
+public class Veld1RijtechniekKeren extends VBox implements View{
 
     private final SchermController schermController;
     private EvaController evaController;
     private LeerlingController llnController;
-    private Button aandachtBtn,exit;
+    private Button aandachtBtn, exit, addButton;
     private TableView<String> list = new TableView<String>();
     private ObservableList<String> houdingen = FXCollections.observableArrayList();
     private Label toon = new Label("Geselecteerd:");
@@ -35,8 +36,11 @@ public class Veld1RijtechniekKeren extends Pane implements View{
     private VBox vBox2 = new VBox();
     private HBox hBox1 = new HBox();
     private HBox hBox2 = new HBox();
+    private GridPane gp = new GridPane();
     private Label titel = new Label("Keren");
     private ImageView titelAfb = new ImageView("resource/Rijtechniek/kerenNeutraal.png");
+    private TextField commentaarFld;
+    private TableColumn commentaarCol;
 
     private TableView<Rijtechniek> table = new TableView<Rijtechniek>();
     private ObservableList<Rijtechniek> data = FXCollections.observableArrayList();
@@ -58,8 +62,7 @@ public class Veld1RijtechniekKeren extends Pane implements View{
             }
         });
 
-        TableColumn commentaarCol = new TableColumn("Commentaar");
-        commentaarCol.setMinWidth(400);
+        commentaarCol = new TableColumn("Commentaar");
         commentaarCol.setCellValueFactory(
                 new PropertyValueFactory<Rijtechniek, String>("commentaar"));
         commentaarCol.setCellFactory(TextFieldTableCell.forTableColumn());
@@ -77,11 +80,10 @@ public class Veld1RijtechniekKeren extends Pane implements View{
         table.getColumns().addAll(commentaarCol);
 
         
-        TextField commentaarFld = new TextField();
-        commentaarFld.setMaxWidth(400);
+        commentaarFld = new TextField();
         commentaarFld.setPromptText("Commentaar");
 
-        final Button addButton = new Button("Voeg toe");
+        addButton = new Button("Voeg toe");
         addButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
@@ -102,34 +104,83 @@ public class Veld1RijtechniekKeren extends Pane implements View{
             }
         });
         table.setEditable(true);
-        table.setMaxHeight(250);
 
-        vBox2.getChildren().addAll(table, commentaarFld, addButton,aandachtBtn);
+        vBox2.getChildren().addAll(table, commentaarFld);
         exit = new Button("ga terug");
-        exit.setTranslateY(12);
 
         exit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                evaController.saveListDataRijtechniek("Achteruit", data);
+                evaController.saveListDataRijtechniek("Keren", data);
                 Veld1RijtechniekKeren.this.schermController.setScherm(MainApp.RIJTECHNIEK_ID);
             }
         });
+        
+        hBox1.getChildren().addAll(titelAfb, titel);
+        hBox1.setMinHeight(50);
+        hBox1.setMaxHeight(50);
+        hBox1.setMinWidth(600);
+        hBox1.setMaxWidth(600);
+        hBox1.setSpacing(15);
+        hBox1.setAlignment(Pos.CENTER);
         titelAfb.setFitHeight(50);
         titelAfb.setFitWidth(50);
-        titel.setMinSize(100, 50);
+        titel.setStyle("-fx-font: 40px Tahoma");
+        toon.setMinHeight(400);
+        toon.setMaxHeight(400);
+        toon.setMinWidth(300);
+        toon.setMaxWidth(300);
+        toon.setAlignment(Pos.TOP_LEFT);
+        table.setMinWidth(300);
+        table.setMaxWidth(300);
+        table.setMinHeight(400);
+        table.setMaxHeight(400);
+        addButton.setMinHeight(30);
+        addButton.setMaxHeight(30);
+        addButton.setMinWidth(150);
+        addButton.setMaxWidth(150);
+        addButton.setAlignment(Pos.CENTER);
+        aandachtBtn.setMinHeight(30);
+        aandachtBtn.setMaxHeight(30);
+        aandachtBtn.setMinWidth(150);
+        aandachtBtn.setMaxWidth(150);
+        aandachtBtn.setAlignment(Pos.CENTER);
+        exit.setMinHeight(30);
+        exit.setMaxHeight(30);
+        exit.setMinWidth(150);
+        exit.setMaxWidth(150);
+        exit.setAlignment(Pos.CENTER);
+        commentaarFld.setMinHeight(30);
+        commentaarFld.setMaxHeight(30);
+        commentaarFld.setMinWidth(250);
+        commentaarFld.setMaxWidth(250);
+        commentaarFld.setAlignment(Pos.CENTER);
         
-        hBox1.getChildren().addAll(exit);
-
-        vBox2.setTranslateX(250);
-        vBox2.setTranslateY(100);
-        vBox1.setTranslateY(100);
-        hBox1.setTranslateY(400);
+        gp.setStyle("-fx-border-color: black;"
+                + "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"
+                + "-fx-background-color: lightgray;"
+                + "-fx-background-radius: 5;");
+        gp.setMaxWidth(900);
+        gp.setMinWidth(900);
+        gp.setMinHeight(600);
+        gp.setMaxHeight(600);
+        gp.setHgap(50);
+        gp.setVgap(20);
+        gp.setAlignment(Pos.CENTER);
+        gp.add(hBox1, 0, 0, 2, 1);
+        gp.add(toon, 0, 1);
+        gp.add(table, 1, 1);
+        gp.add(commentaarFld, 0, 2);
+        gp.add(addButton, 1, 2);
+        gp.add(aandachtBtn, 0, 3);
+        gp.add(exit, 1, 3);
         
-        hBox2.getChildren().addAll(titelAfb, titel);
-
-        this.getChildren().addAll(hBox2, vBox1, vBox2, hBox1);
-        this.setMinWidth(600);
+        
+        this.setAlignment(Pos.CENTER);
+        this.getChildren().add(gp);
+        this.setStyle("-fx-background-image: url('resource/achtergrondStandaard.png')");
+        
+        
         update();
     }
 
@@ -145,7 +196,7 @@ public class Veld1RijtechniekKeren extends Pane implements View{
     
     @Override
     public void update(){
-        data = evaController.loadListDataRijtechniek("Achteruit");
+        data = evaController.loadListDataRijtechniek("Keren");
         table.setItems(data);
     }
     
@@ -155,3 +206,4 @@ public class Veld1RijtechniekKeren extends Pane implements View{
             llnController.setAandachtsPunt(table.getSelectionModel().getSelectedItem().getCommentaar()); 
     }
 }
+
